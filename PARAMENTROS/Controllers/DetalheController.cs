735833977240ca99a7a45f23b585c0f493932e92
robson rodrigues <robsonrodrigues_12@hotmail.com>
2019-03-28 21:27:00 -0400

    Parametros Aula 28/03

---
+++ PARAMENTROS/Controllers/DetalheController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PARAMENTROS.Controllers
+{
+    public class DetalheController : Controller
+    {
+        // GET: Detalhe
+        public ActionResult Index(int prdId)
+        {
+
+            return View();
+        }
+    }
+}
---
+++ PARAMENTROS/Controllers/ProdutoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PARAMENTROS.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Buscar(int prd)
+        {
+            string retorno = string.Empty;
+            string linkretorno = string.Empty;
+            switch(prd)
+            {
+                case 1:
+                retorno = "Globo.com";
+                    linkretorno = "https://www.globo.com/";
+                    break;
+                case 2:
+                    retorno = "R7.com";
+                    linkretorno = "https://www.r7.com/";
+                    break;
+
+                case 5:
+                    retorno = "iphone";
+                    break;
+                default:
+                    retorno = "Nokia";
+                    break;
+            }
+
+            ViewBag.Produto = retorno;
+            ViewBag.Link = linkretorno;
+            return View();
+        }
+    }
+}
